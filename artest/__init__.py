c1977fadd3a1ec6df26f4cb5c0e2b45f76a7b76c
Chou Hung-Yi <hychou.svm@gmail.com>
2023-11-16 23:12:07 +0800

    bump v0.2.0

--- artest/__init__.py
+++ artest/__init__.py
@@ -9,10 +9,10 @@ Public Objects:
     - autoreg: Decorator for creating regression tests during runtime.
     - automock: Decorator for automatic mocking during test execution.
 
-Version: 0.1.2
+Version: 0.2.0
 """
 
 __all__ = ["autoreg", "automock"]
-__version__ = "0.1.2"
+__version__ = "0.2.0"
 
 from .artest import automock, autoreg
