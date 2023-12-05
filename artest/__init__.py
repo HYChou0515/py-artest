830e9c6b72bbe7f2ddf12fcea2dc83c96afe73f3
Chou Hung-Yi <hychou.svm@gmail.com>
2023-12-05 12:58:28 +0800

    bump v0.3.0

--- artest/__init__.py
+++ artest/__init__.py
@@ -9,10 +9,10 @@ Public Objects:
     - autoreg: Decorator for creating regression tests during runtime.
     - autostub: Decorator for automatic stubbing during test execution.
 
-Version: 0.2.3
+Version: 0.3.0
 """
 
 __all__ = ["autoreg", "autostub"]
-__version__ = "0.2.3"
+__version__ = "0.3.0"
 
 from .artest import autoreg, autostub
